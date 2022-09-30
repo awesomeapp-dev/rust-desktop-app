452ec36b83738f5337f2684666da7fd1a66c81c1
Jeremy Chone <jeremy.chone@gmail.com>
2022-09-29 22:18:06 -0700

    . update version to v0.9.6

--- src-tauri/src/prelude.rs
+++ src-tauri/src/prelude.rs
@@ -10,7 +10,7 @@ pub use crate::error::Error;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-// Generic Wrapper class for newtype pattern, mostly for external type to type From/TryFrom conversions
+// Generic Wrapper tuple struct for newtype pattern, mostly for external type to type From/TryFrom conversions
 pub struct W<T>(pub T);
 
 // from: https://github.com/surrealdb/surrealdb.wasm/blob/main/src/mac/mod.rs
