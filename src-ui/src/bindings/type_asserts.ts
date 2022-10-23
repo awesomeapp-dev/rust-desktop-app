826ac46f57f0cf5816a4a28ccdc699b1520b32bc
Jeremy Chone <jeremy.chone@gmail.com>
2022-10-22 19:39:02 -0700

    . downgraded tsconfig to target:ES2018 for MacOS 12.1

--- src-ui/src/bindings/type_asserts.ts
+++ src-ui/src/bindings/type_asserts.ts
@@ -5,7 +5,7 @@ import { ModelMutateResultData } from './index.js';
 export function ensure_ModelMutateResultData(obj: any): ModelMutateResultData {
   const keys = Object.keys(obj);
   if (keys.length != 1 || keys[0] != "id" || typeof obj["id"] !== "string") {
-    throw new Error("assert ModelMutateResultData failed", obj);
+    throw new Error("assert ModelMutateResultData failed {obj}");
   }
   return obj;
 }
